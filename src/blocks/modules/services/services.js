5d8d3da1aa0804d5a973e13227e90c42d70d4cc9
Skep <madskep@gmail.com>
2020-03-16 00:28:35 +0200

    Added slider into services section. Img fix (shadow del).

--- src/blocks/modules/services/services.js
+++ src/blocks/modules/services/services.js
@@ -1,3 +1,12 @@
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 import 'slick-carousel';
+
+$(document).ready(function () {
+    $('.services__slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
