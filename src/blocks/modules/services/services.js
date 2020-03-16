efd7a0fd8e0f96dea3d440a6f1a14b8e5090eb99
Skep_work <madskep@gmail.com>
2020-03-16 11:15:43 +0200

    Services done

--- src/blocks/modules/services/services.js
+++ src/blocks/modules/services/services.js
@@ -5,8 +5,12 @@ import 'slick-carousel';
 $(document).ready(function () {
     $('.services__slider').slick({
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/svg/arrow_left.svg" alt="left arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/svg/arrow_right.svg" alt="right arrow"></button>',
     });
 });
